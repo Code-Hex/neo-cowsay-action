5cfd3a7c6aef84bf56c7b4ed7bf943e9341e6d10
Kei Kamikawa <code-hex@users.noreply.github.com>
2019-12-13 20:15:08 +0900

    fix

--- src/main.sh
+++ src/main.sh
@@ -19,8 +19,9 @@ function parseInputs {
 }
 
 function installNeoCowsay {
-  file="cowsay_v0.1.0_Linux_x86_64.tar.gz"
-  url="https://github.com/Code-Hex/Neo-cowsay/releases/download/v0.1.0/${file}"
+  version="v0.1.1"
+  file="cowsay_${version}_Linux_x86_64.tar.gz"
+  url="https://github.com/Code-Hex/Neo-cowsay/releases/download/${version}/${file}"
   echo "Downloading Neo-cowsay"
   curl -s -S -L -o /tmp/${file} ${url}
   if [ "${?}" -ne 0 ]; then
